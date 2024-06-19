3bacc0b414c5596e505ef5f44ba23eeddc30bf95
isafiulin <45290994+isafiulin@users.noreply.github.com>
2024-06-19 20:56:23 +0600

    Update _calendar_view.dart

--- lib/src/widgets/_impl/_calendar_view.dart
+++ lib/src/widgets/_impl/_calendar_view.dart
@@ -330,32 +330,16 @@ class _CalendarViewState extends State<_CalendarView> {
             height: (widget.config.controlsHeight ?? _subHeaderHeight),
             child: Row(
               children: <Widget>[
-                if (widget.config.centerAlignModePicker != true) const Spacer(),
-                IconButton(
-                  splashRadius: widget.config.dayMaxWidth != null
-                      ? widget.config.dayMaxWidth! * 2 / 3
-                      : null,
-                  icon: widget.config.lastMonthIcon ??
-                      const Icon(Icons.chevron_left),
-                  color: controlColor,
-                  tooltip: _isDisplayingFirstMonth
-                      ? null
-                      : _localizations.previousMonthTooltip,
-                  onPressed:
-                      _isDisplayingFirstMonth ? null : _handlePreviousMonth,
-                ),
-                if (widget.config.centerAlignModePicker == true) const Spacer(),
-                IconButton(
-                  splashRadius: widget.config.dayMaxWidth != null
-                      ? widget.config.dayMaxWidth! * 2 / 3
-                      : null,
-                  icon: widget.config.nextMonthIcon ??
-                      const Icon(Icons.chevron_right),
-                  color: controlColor,
-                  tooltip: _isDisplayingLastMonth
-                      ? null
-                      : _localizations.nextMonthTooltip,
-                  onPressed: _isDisplayingLastMonth ? null : _handleNextMonth,
+                const Spacer(),
+                TextButton(
+                  Text: Text('Cancel', style:TextStyle(
+                    color: widget.config.selectedDayHighlightColor ??
+                        colorScheme.primary,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 14,
+                  ),),
+               
+                  onPressed: () {Navigator.of(context).pop();},
                 ),
               ],
             ),
